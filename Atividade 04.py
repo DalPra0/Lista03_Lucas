6b05242f1c8ed98419da698daa2e49f1e139e4ac
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2024-03-28 20:55:45 -0300

    Codigo atualizado para utilizar condicoes

--- Atividade 04.py
+++ Atividade 04.py
@@ -1,15 +1,15 @@
 const = 1
 n = 0
 quantidade = int(input('Digite a quantidade de numeros: '))
-soma = 0  # Corrigido para iniciar em 0 ao invés de usar int()
+soma = 0
 
-while n < quantidade:  # Corrigido para usar operador de comparação correto
+while n < quantidade and const <= quantidade:
     numero = float(input(f'Digite o numero {const}: '))
     if numero >= 10:
         soma += numero
         n += 1
-        const += 1
     else:
         print('Não aceita numero menor que 10')
+    const += 1
 
 print(f'A soma dos numeros resulta em: {soma}')
